15cd27a6e325f48b7e1cfe0cabacccd1532dcee7
branquinhoaa <amanda.alves15@gmail.com>
2018-03-23 09:25:41 -0300

    acccuracy measure for decision tree

--- validate_poi.py
+++ validate_poi.py
@@ -2,20 +2,14 @@
 
 
 """
-    Starter code for the validation mini-project.
-    The first step toward building your POI identifier!
-
-    Start by loading/formatting the data
-
-    After that, it's not our code anymore--it's yours!
+    In this code
 """
 
 import pickle
 import sys
-sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("final_project/final_project_dataset.pkl", "r") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -25,8 +19,27 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+################################
+# here I split the data into test and training data
 
+from sklearn.cross_validation import train_test_split
 
-### it's all yours from here forward!  
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+################################
+# here I create my decision tree classifier
+
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(features_train, labels_train)
+
+accuracy = clf.score(features_test, labels_test)
+
+# here I show the accuracy of my data:
+
+print("accuracy: ", accuracy)
+
 
 
